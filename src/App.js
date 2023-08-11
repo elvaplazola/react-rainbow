ba2c5bd83dc5c73e5d369e8a76f2199900f905ff
Elva Plazola <elvaplazola@yahoo.com>
2023-08-11 04:31:05 -0700

    render rainbow

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React, { useState } from 'react';
+import ColorBlock from './colorBlock';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
+
+  let colorMap = colors.map((color, i) => {
+    return (
+      <ColorBlock key={i} color={color}></ColorBlock>
+    )
+  })
+
   return (
     <div className="App">
-  
+ {colorMap}
     </div>
   );
 }
